34240a4f60c348b15a034c36ded5b03781fc6d22
richard-clark <rctheslimer@gmail.com>
2014-07-11 17:02:40 -0400

    Updated documentation.

--- pydynamixel/dynamixel.py
+++ pydynamixel/dynamixel.py
@@ -36,30 +36,6 @@ where ``checksum`` is the complement of the lowest byte of the sum of the data b
 
 A number of methods are provided for handling packets.
 
-Examples
---------
-
-The following example turns on the red LED of a servo with id 1 located (on a POSIX-
-compliant system) at ``/dev/tty.usbserial-A9C73L15``:
-
-.. code-block:: python
-
-    import dynamixel
-
-    try:
-        url = '/dev/tty.usbserial-A9C73L15'
-        servo_id = 1
-        num_attempts = 10
-        led_value = 1 # Red
-        
-        ser = dynamixel.get_serial_for_url(url)
-        packet = get_led_packet(servo_id, led_value)
-        write_and_get_response_multiple(ser, packet, num_attempts, led_value)
-        print('LED set successfully!')
-    except Exception as e:
-        print('Unable to set LED.')
-        print(e)
-
 
 """
 
